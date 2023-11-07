f9ab751433a406a6ffce08ca3a96d4e9017147d8
Chenguang Zhao <25994188+czhao2@users.noreply.github.com>
2023-11-07 18:35:41 -0500

    Add files via upload

---
+++ example/test_5_samples.2/psiblast_cmd.sh
@@ -0,0 +1,10 @@
+/home/czhao/tools/blast-2.2.23/bin/blastpgp -i data-jobs/202103/test_5_samples.2/psiblast_run//T100900000046.fa -d data-cafa/train_data.fa -j 2 -b 1000 -o data-jobs/202103/test_5_samples.2/psiblast_run//T100900000046.fa2
+perl parse_blast_result_2.pl data-jobs/202103/test_5_samples.2/psiblast_run//T100900000046.fa2 data-jobs/202103/test_5_samples.2/psiblast_run//T100900000046.fa3 2
+/home/czhao/tools/blast-2.2.23/bin/blastpgp -i data-jobs/202103/test_5_samples.2/psiblast_run//T100900000115.fa -d data-cafa/train_data.fa -j 2 -b 1000 -o data-jobs/202103/test_5_samples.2/psiblast_run//T100900000115.fa2
+perl parse_blast_result_2.pl data-jobs/202103/test_5_samples.2/psiblast_run//T100900000115.fa2 data-jobs/202103/test_5_samples.2/psiblast_run//T100900000115.fa3 2
+/home/czhao/tools/blast-2.2.23/bin/blastpgp -i data-jobs/202103/test_5_samples.2/psiblast_run//T100900000116.fa -d data-cafa/train_data.fa -j 2 -b 1000 -o data-jobs/202103/test_5_samples.2/psiblast_run//T100900000116.fa2
+perl parse_blast_result_2.pl data-jobs/202103/test_5_samples.2/psiblast_run//T100900000116.fa2 data-jobs/202103/test_5_samples.2/psiblast_run//T100900000116.fa3 2
+/home/czhao/tools/blast-2.2.23/bin/blastpgp -i data-jobs/202103/test_5_samples.2/psiblast_run//T100900000141.fa -d data-cafa/train_data.fa -j 2 -b 1000 -o data-jobs/202103/test_5_samples.2/psiblast_run//T100900000141.fa2
+perl parse_blast_result_2.pl data-jobs/202103/test_5_samples.2/psiblast_run//T100900000141.fa2 data-jobs/202103/test_5_samples.2/psiblast_run//T100900000141.fa3 2
+/home/czhao/tools/blast-2.2.23/bin/blastpgp -i data-jobs/202103/test_5_samples.2/psiblast_run//T100900000026.fa -d data-cafa/train_data.fa -j 2 -b 1000 -o data-jobs/202103/test_5_samples.2/psiblast_run//T100900000026.fa2
+perl parse_blast_result_2.pl data-jobs/202103/test_5_samples.2/psiblast_run//T100900000026.fa2 data-jobs/202103/test_5_samples.2/psiblast_run//T100900000026.fa3 2
