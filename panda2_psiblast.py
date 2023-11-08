b184bf4a9430a001c33f1318e0019299c4761787
Chenguang Zhao <25994188+czhao2@users.noreply.github.com>
2023-11-07 23:39:56 -0500

    Update panda2_psiblast.py

--- panda2_psiblast.py
+++ panda2_psiblast.py
@@ -4,6 +4,8 @@ import numpy as np
 
 # Run psiblast and parser
 def df_psiblast(fasta_dir, df):
+    blast_path = "" # /blast-2.2.23/bin/blastpgp
+    /home/czhao/tools/blast-2.2.23/bin/blastpgp
     if fasta_dir[-1] == '/': fasta_dir = fasta_dir[:-1]
     psiblast_dir = f'{fasta_dir}.2/psiblast_run/'
     results = []
@@ -15,7 +17,7 @@ def df_psiblast(fasta_dir, df):
             with open(fa_f,'w') as fa_fh:
                 fa_fh.write(f'>{prot}\n{seq}\n')
             if not os.path.exists(fa_f+'2'):
-                w_fh.write(f'/home/czhao/tools/blast-2.2.23/bin/blastpgp -i {fa_f} -d data-cafa/train_data.fa -j 2 -b 1000 -o {fa_f}2\n')
+                w_fh.write(f'{blast_path} -i {fa_f} -d data-cafa/train_data.fa -j 2 -b 1000 -o {fa_f}2\n')
             if not os.path.exists(fa_f+'3'):
                 w_fh.write(f'perl parse_blast_result_2.pl {fa_f}2 {fa_f}3 2\n')
             results.append(f'{fa_f}3')
